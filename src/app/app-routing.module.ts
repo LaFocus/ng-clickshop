170c8b35a2c8ea525524427ae70d9285377c9b42
Focus <daniyar.danilov@gmail.com>
2024-01-10 17:48:00 +0500

    header added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 export const routes: Routes = [
-  {
-    path: '/main'
-  }
+  
 ];
 
 @NgModule({
--- src/app/components/Header/header.component.ts
+++ src/app/components/Header/header.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  counter: number = 1
 }
