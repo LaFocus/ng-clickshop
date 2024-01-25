74d373c5c28d33c8a180c86422be21ce190341f8
Focus <daniyar.danilov@gmail.com>
2024-01-25 17:18:49 +0500

    selected items page completed

--- src/app/components/selected/selected.component.ts
+++ src/app/components/selected/selected.component.ts
@@ -1,6 +1,9 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SelectedItems } from '../../services/products/selectedItems.service';
+import { of } from 'rxjs';
+
 @Component({
   selector: 'app-selected',
   standalone: true,
@@ -8,10 +11,29 @@ import { RouterLink } from '@angular/router';
   templateUrl: './selected.component.html',
   styleUrl: './selected.component.scss'
 })
+
 export class SelectedComponent {
   selectedProducts: any[] = []
-  ngOnInit() {
-    this.selectedProducts = JSON.parse(localStorage.getItem('selected') || '[]') 
+
+
+  getSelectedItems() {
+    of(this.selectedService.selected).subscribe((observer) => {
+      this.selectedProducts = observer
+    })
   }
 
+  addItemToSelected(item: any) {
+    this.selectedService.addToSelected(item)
+    this.getSelectedItems()
+  }
+
+
+  ngOnInit() {
+    this.selectedService.getSelectedItems()
+    this.getSelectedItems()
+  }
+
+  constructor(
+    private selectedService: SelectedItems
+  ) {}
 }
--- src/app/services/products/selectedItems.service.ts
+++ src/app/services/products/selectedItems.service.ts
@@ -4,10 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class SelectedItems {
+  selected: any;
+
+  getSelectedItems() {
+    this.selected = JSON.parse(localStorage.getItem('selected') || '[]')
+  }
+
   addToSelected(item: any) {
-    let selected: any;
-    selected = JSON.parse(localStorage.getItem('selected') || '[]');
-    selected.push(item);
-    localStorage.setItem('selected', JSON.stringify(selected));
+    this.selected = JSON.parse(localStorage.getItem('selected') || '[]');
+
+    for (let i = 0; i <= this.selected.length; i++) {
+      const element = this.selected[i];
+      
+      if (i == this.selected.length) {
+        this.selected.push(item);
+        break;
+      }
+      if (element.id !== item.id) {
+        continue;
+      } else {
+        this.selected.splice(i, 1);
+        break;
+      }
+    }
+
+    localStorage.setItem('selected', JSON.stringify(this.selected));
   }
 }
