c1d7a52375bf04c68fd2668b96c2bc2d306afff6
Focus <daniyar.danilov@gmail.com>
2024-01-26 17:43:54 +0500

    payments + modal

--- src/app/components/payments/payments.component.ts
+++ src/app/components/payments/payments.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart/cart.service';
+import { of } from 'rxjs';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-payments',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './payments.component.html',
   styleUrl: './payments.component.scss'
 })
 export class PaymentsComponent {
 
+  cart: any
+  flag: boolean = false
+
+  getCart() {
+    of(this.cartService.cart).subscribe((observer) => this.cart = observer)
+  }
+
+  ngOnInit() {
+    this.cartService.getcartItems()
+    this.getCart()
+  }
+
+  constructor(
+    private cartService: CartService
+  ) {}
 }
