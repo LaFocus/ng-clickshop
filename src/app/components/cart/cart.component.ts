88f093641e9910d56b9fc42016c727ba4baf9df4
Focus <daniyar.danilov@gmail.com>
2024-01-26 11:11:42 +0500

    cart-item added

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,21 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { register } from 'swiper/element/bundle';
+import { CartService } from '../../services/cart/cart.service';
+import { CartItemComponent } from './cart-item/cart-item.component';
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CartComponent {
-
-
-
   ngOnInit() {
-    register()
+    register();
+    this.cartService.getcartItems()
   }
 
+  constructor(
+    private cartService: CartService
+  ) {}
 }
