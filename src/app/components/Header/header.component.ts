07ae43c5ff7cc8c03299434ff6ca906375307ac9
Focus <daniyar.danilov@gmail.com>
2024-01-30 17:40:06 +0500

    burger menu added

--- src/app/components/Header/header.component.ts
+++ src/app/components/Header/header.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 import { CartService } from '../../services/cart/cart.service';
 import { of } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   counter: number = 0;
+  isBurgerActive: boolean = false
 
   serCounter() {
     this.cartService.getcartItems()
