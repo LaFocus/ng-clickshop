d2a11aaac56dd95c16bdcba569a8de147515c3c8
Focus <daniyar.danilov@gmail.com>
2024-01-31 09:51:53 +0500

    cart icon selection added

--- src/app/components/Main/main-content/main-content-item/main-content-item.component.ts
+++ src/app/components/Main/main-content/main-content-item/main-content-item.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SelectedItems } from '../../../../services/products/selectedItems.service';
 import { CartService } from '../../../../services/cart/cart.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'main-content-item',
@@ -14,7 +15,10 @@ export class MainContentItemComponent {
   @Input() item: any;
   @Input() selected: any;
 
-  selectedIconFill: '#030A8C' | 'none' = 'none'
+  selectedIconFill: '#030A8C' | 'none' = 'none';
+  cartIconFill: '#000000' | '#030A8C' = '#030A8C';
+
+  cart: any;
 
   roundValue = (value: number) => {
     return Math.floor(value);
@@ -22,28 +26,34 @@ export class MainContentItemComponent {
 
   pushToSelected() {
     this.selectedService.addToSelected(this.item);
-    this.changeSelection() 
+    this.selectedIconFill =
+      this.selectedIconFill == '#030A8C' ? 'none' : '#030A8C';
   }
 
   pushToCart() {
     this.cartService.addOrDelete(this.item);
+    this.cartIconFill = this.cartIconFill == '#030A8C' ? '#000000' : '#030A8C';
   }
 
-  checkForSelection() {
+  checkForSelectionAndCart() {
     for (let i = 0; i < this.selected.length; i++) {
       const element = this.selected[i];
       if (element.id == this.item.id) {
-        this.selectedIconFill = '#030A8C'
+        this.selectedIconFill = '#030A8C';
+      }
+    }
+    for (let i = 0; i < this.cart.products.length; i++) {
+      const element = this.cart.products[i];
+      if (element.id == this.item.id) {
+        this.cartIconFill = '#000000';
       }
     }
   }
 
-  changeSelection() {
-    this.selectedIconFill = this.selectedIconFill == '#030A8C' ? 'none' : '#030A8C'
-  }
-
   ngOnInit() {
-    this.checkForSelection()
+    of(this.cartService.cart).subscribe((observer) => this.cart = observer)
+    this.checkForSelectionAndCart();
+    this.cartService.getcartItems();
   }
 
   constructor(
